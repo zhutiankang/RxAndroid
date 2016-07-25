dc7fa4c7bfdc00192b8abc10acf7a7108b30a911
ztk2020 <ztk1993@163.com>
2016-07-25 11:03:42 +0800

    订阅集合应用与bundle应用

--- app/src/main/java/com/tiankang/rxandroid/MainActivity.java
+++ app/src/main/java/com/tiankang/rxandroid/MainActivity.java
@@ -10,44 +10,78 @@ import com.tiankang.rxandroid.rxbus.RxBus;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import rx.functions.Action1;
+import rx.functions.Func1;
+import rx.subscriptions.CompositeSubscription;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RxBus _rxBus;
+    private RxBus _rxBus = RxBus.getInstance();
+    private CompositeSubscription subscription;
 
-    public RxBus getRxBusSingleTon(){
-        if (_rxBus == null){
-            _rxBus = new RxBus();
-        }
-        return _rxBus;
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        getRxBusSingleTon();
-
-        _rxBus.toObservable().subscribe(new Action1<Object>() {
+        subscription = new CompositeSubscription();
+        subscription.add(_rxBus.toObserverable().subscribe(new Action1<Object>() {
             @Override
             public void call(Object event) {
-                if (event instanceof KissEvent){
+                if (event instanceof KissEvent) {
                     showToast();
                 }
             }
-        });
+        }));
     }
 
     private void showToast() {
-        Toast.makeText(this,"Kiss me",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Kiss me", Toast.LENGTH_SHORT).show();
     }
 
     @OnClick(R.id.btn_kiss)
-    public void kissMe(){
-        _rxBus.send(new KissEvent());
+    public void kissMe() {
+        if (_rxBus.hasObservers()) {
+            _rxBus.send(new KissEvent());
+        }
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        subscription.clear();
+    }
 
+    /*----------------------------------Bundle应用 舍弃setArgment-------------------------------------------------------------*/
 
+    public void send(Bundle bundle) {
+        bundle.putString("eventname", "addOrderInfoGoods");
+        _rxBus.send(bundle);
+    }
+
+    /**
+     * 首先使用map进行类型转换，将Object类型转换为Bundle类型
+     * 之后在onNext中进行处理（这里使用一个参的new Action1替换）
+     */
+    public void operateBundle() {
+
+        subscription.add(_rxBus.toObserverable()
+                .map(new Func1<Object, Bundle>() {
+                    @Override
+                    public Bundle call(Object o) {
+                        return (Bundle) o;
+                    }
+                })
+                .subscribe(new Action1<Bundle>() {
+                    @Override
+                    public void call(Bundle bundle) {
+                        if (bundle.getString("eventname").equals("addOrderInfoGoods")) {
+                            //show(bundle);
+                        }
+                        if (bundle != null){
+                            //handle(bundle)
+                        }
+                    }
+                }));
+    }
 
 }
--- app/src/main/java/com/tiankang/rxandroid/rxbus/RxBus.java
+++ app/src/main/java/com/tiankang/rxandroid/rxbus/RxBus.java
@@ -12,14 +12,27 @@ public class RxBus {
 
     private final Subject<Object,Object> _bus = new SerializedSubject<>(PublishSubject.create());
 
+    private RxBus(){};
+
+    public static RxBus getInstance(){
+        return RxBusHolder.instance;
+    }
+    public static class RxBusHolder{
+        public static final RxBus instance = new RxBus();
+    }
+
+
     public void send(Object o){
         _bus.onNext(o);
     }
 
-    public Observable<Object> toObservable(){
+    public Observable<Object> toObserverable(){
         return _bus;
     }
 
+    /**与send结合用，判断是否有观察者，没有不发事件
+     * @return
+     */
     public boolean hasObservers(){
         return _bus.hasObservers();
     }
